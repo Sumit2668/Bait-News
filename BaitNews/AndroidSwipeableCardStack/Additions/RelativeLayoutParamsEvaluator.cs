a6055a1e282bf71a4ac9f679d14305ee08f83154
James Montemagno <james.montemagno@gmail.com>
2016-08-07 11:43:34 -0700

    Artwork, CardView Swipe Control, and Android setup
    More details in the actual main activity and all the resources needed.
    
    Also added in some icons for Android

---
+++ BaitNews/AndroidSwipeableCardStack/Additions/RelativeLayoutParamsEvaluator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.Animation;
+using Java.Lang;
+
+namespace Com.Wenchao.Animation
+{
+	public partial class RelativeLayoutParamsEvaluator : Java.Lang.Object, ITypeEvaluator
+	{
+
+		public Java.Lang.Object Evaluate(float fraction, Java.Lang.Object startValue, Java.Lang.Object endValue)
+		{
+			return null;
+		}
+	}
+}
+
--- BaitNews/BaitNews.Android/MainActivity.cs
+++ BaitNews/BaitNews.Android/MainActivity.cs
@@ -1,26 +1,74 @@
 ﻿using Android.App;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Com.Wenchao.Cardstack;
 
 namespace BaitNews.Droid
 {
-	[Activity(Label = "Bait News", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+	[Activity(Label = "Bait News", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+	public class MainActivity : BaseActivity
+    {
 
-		protected override void OnCreate(Bundle savedInstanceState)
+        protected override int LayoutResource => Resource.Layout.main;
+
+		CardStack cardStack;
+		CardsDataAdapter adapter;
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate(savedInstanceState);
+			base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+			cardStack = FindViewById<CardStack>(Resource.Id.container);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			cardStack.SetContentResource(Resource.Layout.item_card);
+			cardStack.SetStackMargin(20);
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+			adapter = new CardsDataAdapter(this, Resource.Id.content, new string[]
+			{
+				"Test1", "Test2", "Test3", "Test4", "Test5"
+			});
+
+
+			cardStack.Adapter = adapter;
+
+			SupportActionBar?.SetDisplayHomeAsUpEnabled(false);
+			SupportActionBar?.SetHomeButtonEnabled(false);
+		}
+
+
+		class CardsDataAdapter : ArrayAdapter<string>
+		{
+			public CardsDataAdapter(Context context, int textViewResourceId, string[] objects)
+				: base(context, textViewResourceId, objects)
+			{
+			}
+
+			public override Android.Views.View GetView(int position, Android.Views.View convertView, Android.Views.ViewGroup parent)
+			{
+				CardsViewHolder holder = null;
+				var view = convertView;
+				holder = view?.Tag as CardsViewHolder;
+
+				if (holder == null)
+				{
+					holder = new CardsViewHolder();
+					view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.item_card, parent, false);
+
+					holder.Content = view.FindViewById<TextView>(Resource.Id.content);
+					view.Tag = holder;
+				}
+
+				holder.Content.Text = GetItem(position);
+
+				return view;
+			}
+		}
+
+		class CardsViewHolder : Java.Lang.Object
+		{
+			public TextView Content { get; set; }
 		}
 	}
 }
